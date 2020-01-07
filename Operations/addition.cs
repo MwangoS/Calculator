ba4cd0cb34d14f8520783765282f4d52d7e6d9ed
MwangoS <natchaziya@gmail.com>
2020-01-07 14:16:34 +0200

    Add project files.

---
+++ Operations/addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operations
+{
+    public class Addition
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        
+            }
+
+    
+}
