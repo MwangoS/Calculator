0c57c9a7d66541695ad673a3741c3a849643582f
MwangoS <41151559+MwangoS@users.noreply.github.com>
2020-01-10 17:09:01 +0200

    Fix: Updated Multiplication code

--- Multiply/Multiply.cs
+++ Multiply/Multiply.cs
@@ -6,12 +6,12 @@ namespace Multiply
 {
     public interface IMultiply
     {
-        public dynamic Subtraction(dynamic a, dynamic b); 
+        public dynamic Multiplication(dynamic a, dynamic b); 
     }
     public class Multiple : IMultiply
     {
         public dynamic result;        
-        public dynamic Subtraction(dynamic a, dynamic b)
+        public dynamic Multiplication(dynamic a, dynamic b)
         {
             result = Operations.Multiplication.Multiply(a, b);
             return result;
